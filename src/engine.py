b424e0402e1ef86c40552e61130dcf0191d7e790
alexxanton <alexantonaranda@gmail.com>
2026-08-12 00:36:25 +0200

    implemented constrained decoding logic

--- src/engine.py
+++ src/engine.py
@@ -1,9 +1,10 @@
 import json
 import numpy as np
-from typing import List, Dict
+from typing import List
 from pydantic import BaseModel
 from .validation import Function
 from .base_prompt import BasePrompt
+from .decoder import ConstrainedDecoder
 
 
 class FunctionCallingEngine(BaseModel):
@@ -18,30 +19,58 @@ class FunctionCallingEngine(BaseModel):
         """Initialize the function calling engine."""
         super().__init__(functions=functions)
         self._base_prompt = BasePrompt(functions)
+
         from llm_sdk.llm_sdk import Small_LLM_Model
         self._model = Small_LLM_Model()
-        self._vocab: Dict[str, int] = {}
-        self._load_vocab()
-
-    def _load_vocab(self) -> None:
-        """Load vocabulary."""
         path = self._model.get_path_to_vocab_file()
         with open(path, "r", encoding="utf-8") as f:
             self._vocab = json.load(f)
+        self._id_values = {v: k for k, v in self._vocab.items()}
+
+    def _get_input_ids(self, output: str) -> List[int]:
+        """Encode a string and return as a list of ints."""
+        return self._model.encode(output)[0].tolist()
 
     def generate(self, prompt: str) -> str:
         """Generate the output."""
-        prompt = f'{{\n  "prompt": "{prompt}",\n  "name": "'
-        output = f'{self._base_prompt}\n{prompt}'
-        print(prompt, end="")
-        input_ids = self._model.encode(output)[0].tolist()
-        #print(input_ids)
-        token = ""
-        while "}\n" not in token:
+        decoder = ConstrainedDecoder(prompt, self.functions)
+        input_ids = self._get_input_ids(f'{self._base_prompt}\n')
+        output_start_idx = len(self._model.decode(input_ids))
+        input_ids += self._get_input_ids(f"{decoder.state}")
+        name_complete = False
+        text_idx = output_start_idx
+        param_complete = True
+
+        while not decoder.finished:
+            if not name_complete:
+                allowed_tokens = decoder.get_name_tokens(
+                    self._model.decode(input_ids), self._vocab, self._id_values
+                )
+            elif not decoder.func_name:
+                decoder.retrieve_func_name(self._model.decode(input_ids))
+                input_ids += self._get_input_ids(decoder.state)
+
+            if name_complete and param_complete:
+                input_ids += self._get_input_ids(decoder.inject_next_param())
+                param_complete = False
+
             logits = self._model.get_logits_from_input_ids(input_ids)
-            logits_arr = np.array(logits)
-            next_id = int(np.argmax(logits_arr))
+            np_logits = np.array(logits)
+            if allowed_tokens:
+                mask = np.ones_like(np_logits, dtype=bool)
+                for allowed in allowed_tokens:
+                    if 0 <= allowed < len(mask):
+                        mask[allowed] = False
+                np_logits[mask] = -float("inf")
+                allowed_tokens = None
+
+            next_id = int(np.argmax(np_logits))
+            if next_id == self._vocab.get('"'):
+                name_complete = True
             input_ids.append(next_id)
-            token = self._model.decode([next_id])
-            print(token, end="", flush=True)
+
+            text = self._model.decode(input_ids)
+            print(text[text_idx:], end="", flush=True)
+            text_idx = len(text)
+
         return self._model.decode(input_ids)
