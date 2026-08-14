5c2e80529414dc6e1bb31d58c8b9d8c8843efec0
alexxanton <alexantonaranda@gmail.com>
2026-08-15 01:26:30 +0200

    implemented parameter finalization methods

--- src/decoder.py
+++ src/decoder.py
@@ -1,6 +1,7 @@
+import re
 from pydantic import BaseModel
 from .validation import Function, Parameter
-from typing import List, Iterator, Dict, Set, Tuple
+from typing import List, Iterator, Dict, Set, Tuple, Optional
 
 
 class ConstrainedDecoder(BaseModel):
@@ -21,38 +22,35 @@ class ConstrainedDecoder(BaseModel):
             '  }\n}'
         ])
         self._state = next(self._states)
+        self._next_param: Optional[Parameter] = None
         self._finished = False
         self._func_name = ""
         self._last_param_reached = False
+        self._params = None
 
     def _get_params(self, name: str) -> Iterator[Tuple[str, Parameter]]:
         """Get parameters from function."""
         func = next((f for f in self.functions if f.name == name), None)
         if func is None:
             return iter([])
-        return iter(func.parameters.items())
+        self._params = iter(func.parameters.items())
+        self._next_param = next(self._params)
 
     def retrieve_func_name(self, output: str) -> None:
         """Get function name from generated output."""
         name = output.split(self._prefix)[-1][:-1]
         self._state = next(self._states)
-        self._params = self._get_params(name)
+        self._get_params(name)
         self._func_name = name
 
-    def inject_next_param(self) -> str:
-        """Inject the next parameter from the function."""
-        param = next(self._params, None)
-        if param is None:
-            self._finished = True
-            return ""
-        formatted_param = (
-            f'    "{param[0]}": ' +
-            ('"' if param[1].type == "string" else "")
-        )
-        return formatted_param
+    def get_num_tokens(
+        self, vocab: Dict[str, int], id_values: Dict[int, str]
+    ) -> Set[int]:
+        """Get alowed tokens for number parameters."""
+        allowed = set()
 
     def get_name_tokens(
-            self, output: str, vocab: Dict[str, int], id_values: Dict[int, str]
+        self, output: str, vocab: Dict[str, int], id_values: Dict[int, str]
     ) -> Set[int]:
         """Get allowed tokens for function name."""
         name = output.split(self._prefix)[-1]
@@ -69,6 +67,25 @@ class ConstrainedDecoder(BaseModel):
                 if dquote is not None:
                     allowed.add(dquote)
         return allowed
+
+    def inject_next_param(self) -> str:
+        """Inject the next parameter from the function."""
+        param = self._next_param
+        self._next_param = next(self._params, None)
+        if self._next_param is None:
+            self._last_param_reached = True
+        formatted_param = (
+            f'    "{param[0]}": ' +
+            ('"' if param[1].type == "string" else "")
+        )
+        #print("(OK)", end="")
+        return formatted_param
+
+    def close_param(self) -> None:
+        """Finalize parameter generation."""
+        if self._last_param_reached:
+            self._finished = True
+            self._state = next(self._states)
 
     @property
     def state(self) -> str:
